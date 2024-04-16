ae67287e55331e722d7761ddb0d888c389f941ad
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-04-16 18:52:10 -0400

    Finished wiring up code.

--- people/aarone/aaronmain.js
+++ people/aarone/aaronmain.js
@@ -1,1 +1,7 @@
-const favoriteBtn = document.querySelector("favorite-btn");
+const favoriteBtn = document.querySelector(".favorite-btn");
+const favoriteItems = " I enjoy memes, video games, music, and programming.";
+//https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+favoriteBtn.addEventListener("click",(e)=>{
+    console.log("Hello");
+    alert(favoriteItems);
+});
